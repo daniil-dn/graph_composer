a04dd4934df2862ca6e99cddba19e53f84f572dc
daniildn <dosindaniilm@gmail.com>
2022-01-06 20:38:51 +0300

    completed with texts

--- compose.py
+++ compose.py
@@ -1,15 +1,60 @@
+import random
+import string
+import re
+import os
+from graph import Vertex, Graph
+
+
 def get_words_from_text(text_path):
     with open(text_path, 'r') as f:
         text = f.read()
+        text = re.sub(r'\[(.+)\]', ' ', text)
         text = ' '.join(text.split())  # whitespaces to spaces
+        text = text.lower()
+        text = text.translate(str.maketrans('', '', string.punctuation))
+    words = text.split()
+    return words
+
+
+def make_graph(words):
+    previous_word = None
+    g = Graph()
+    # for each word check that word is in the graph, and if not then add it
+    for word in words:
+        word_vertex = g.get_vertex(word)
+        if previous_word:
+            previous_word.increment_edge(word_vertex)
+        previous_word = word_vertex
+
+    g.generate_probability_mappings()
+    return g
+
+    # if there was a porevious word, then add an edge if it does not already exist
+    # in the graph, otherwise increment weight by 1
+    # set our word to the previous word and iterate!
+
+    # now remeber that we want to generate the probablility mappings before composing
+    # this is a great place to do it before we return the graph object
+
+
+def compose(g, words, length=50):
+    composition = []
+    word = g.get_vertex(random.choice(words))
+    for _ in range(length):
+        composition.append(word.value)
+        word = g.get_next_word(word)
+
+    return composition
 
 
 def main():
-    """
-    1:get words from text
-    2:make agraph using those words
-    3:get the next word for x number of words (defined by user)
-    4:show the user!
-    :return:
-    """
-    pass
+    words = get_words_from_text('texts/hp_sorcerer_stone.txt')
+    # for song_file in os.listdir(f'songs/{artist}'):
+
+    g = make_graph(words)
+    composition = compose(g, words, 100)
+    print(' '.join(composition))
+
+
+if __name__ == '__main__':
+    main()
--- graph.py
+++ graph.py
@@ -8,43 +8,46 @@ class Vertex:
         self.neighbors = []
         self.neighbors_weights = []
 
+    def __str__(self):
+        return self.value + ' '.join([node.value for node in self.adjacent.keys()])
+
     def add_edge_to(self, vertex, weight=0):
-        """this is adding an edge to the vertex we input with weight"""
         self.adjacent[vertex] = weight
 
     def increment_edge(self, vertex):
-        """this is incrementing the weight of the edge"""
         self.adjacent[vertex] = self.adjacent.get(vertex, 0) + 1
 
+    def get_adjacent_nodes(self):
+        return self.adjacent.keys()
+
+    # initializes probability map
     def get_probability_map(self):
         for (vertex, weight) in self.adjacent.items():
             self.neighbors.append(vertex)
             self.neighbors_weights.append(weight)
 
-    def next_word(self, value):
-        """randomly select next word ***based on weiths!!!"""
+    def next_word(self):
         return random.choices(self.neighbors, weights=self.neighbors_weights)[0]
 
-    class Graph:
-        def __init__(self):
-            self.vertices = {}
 
-        def get_vertex_values(self):
-            """return all possible words"""
-            return set(self.vertices.keys())
+class Graph(object):
+    def __init__(self):
+        self.vertices = {}
 
-        def add_vertex(self, value):
-            self.vertices[value] = Vertex(value)
+    def get_vertex_values(self):
+        return set(self.vertices.keys())
 
-        def get_vertex(self, value):
-            """what if the value isnt in the graph?"""
-            if value not in self.vertices:
-                self.add_vertex(value)
-            return self.vertices[value]
+    def add_vertex(self, value):
+        self.vertices[value] = Vertex(value)
 
-        def get_next_word(self, current_vertex):
-            self.vertices[current_vertex.value].next_word()
+    def get_vertex(self, value):
+        if value not in self.vertices:
+            self.add_vertex(value)
+        return self.vertices[value]
 
-        def generate_probability_mappings(self):
-            for vertex in self.vertices.values():
-                vertex.get_probability_map()
+    def get_next_word(self, current_vertex):
+        return self.vertices[current_vertex.value].next_word()
+
+    def generate_probability_mappings(self):
+        for vertex in self.vertices.values():
+            vertex.get_probability_map()
